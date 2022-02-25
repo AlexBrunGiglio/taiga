d0f340769ec43e8a55f3450f3fbea5b2785a3693
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-25 08:47:43 +0100

    basemodulelist

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,9 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { TuiDialogModule, TuiNotificationsModule, TuiRootModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiDialogModule, TuiLoaderModule, TuiModeModule, TuiNotificationModule, TuiNotificationsModule, TuiRootModule, TuiThemeNightModule } from '@taiga-ui/core';
+import { TuiActionModule, TuiInputModule, TuiInputPhoneInternationalModule, TuiInputPhoneModule } from '@taiga-ui/kit';
+import { environment } from '../environments/environment';
+import { BASE_PATH, Configuration, ConfigurationParameters } from '../providers/api-client.generated';
 
 import { AppRoutingModule } from '../routes/app-routing.module';
+import { AuthGuard } from '../routes/guards/auth.guard';
+import { RoleGuard } from '../routes/guards/roles.guard';
+import { HttpInterceptor } from '../utils/http-interceptor';
+import { AuthProvider } from '../utils/services/auth-provider';
 import { AppComponent } from './app.component';
+
+
+export function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    apiKeys: {},
+    withCredentials: true,
+  };
+  return new Configuration(params);
+}
+
+export const BasePageModulesList = [
+  CommonModule,
+  ReactiveFormsModule,
+  TuiInputModule,
+  TuiInputPhoneModule,
+  TuiInputPhoneInternationalModule,
+  FormsModule,
+  TuiLoaderModule,
+  TuiNotificationModule,
+  TuiThemeNightModule,
+  TuiModeModule,
+  TuiDialogModule,
+  TuiActionModule,
+  TuiButtonModule,
+  TuiNotificationsModule
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +52,13 @@ import { AppComponent } from './app.component';
     TuiNotificationsModule,
     TuiDialogModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    RoleGuard,
+    AuthProvider,
+    { provide: BASE_PATH, useValue: environment.apiBaseUrl },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
