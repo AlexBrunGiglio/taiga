6d358369e32f7b5e88b1f35261527bf0f385829a
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-02-25 09:31:08 +0100

    routing

--- front/src/routes/app-routing.module.ts
+++ front/src/routes/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { RoutesList } from './routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: RoutesList.Home,
+    pathMatch: 'full'
+  },
+  {
+    path: RoutesList.Home,
+    loadChildren: () => import('../pages/public/home/home.module').then(m => m.HomeModule),
+    pathMatch: 'full',
+    canActivate: [],
+  },
+  {
+    path: RoutesList.Register,
+    loadChildren: () => import('../pages/auth/register/register.module').then(m => m.RegisterModule),
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
