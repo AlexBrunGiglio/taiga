28d2308149b2bf70b78df73388fc3b008842a06c
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-03-09 14:27:56 +0100

    improve drawer admin

--- front/src/components/admin-drawer/admin-drawer.component.ts
+++ front/src/components/admin-drawer/admin-drawer.component.ts
@@ -4,6 +4,7 @@ import { BaseComponent } from '../../utils/base/base.component';
 @Component({
     selector: 'app-admin-drawer',
     templateUrl: './admin-drawer.component.html',
+    styleUrls: ['./admin-drawer.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
 export class AdminDrawerComponent extends BaseComponent {
--- front/src/components/admin-drawer/admin-navbar/admin-navbar.component.ts
+++ front/src/components/admin-drawer/admin-navbar/admin-navbar.component.ts
@@ -4,7 +4,6 @@ import { BaseComponent } from '../../../utils/base/base.component';
 @Component({
     selector: 'app-admin-navbar',
     templateUrl: './admin-navbar.component.html',
-    styleUrls: ['./admin-navbar.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
 export class AdminNavbarComponent extends BaseComponent {
