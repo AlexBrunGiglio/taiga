e82acd9d78ed46135fbabf5880c7c6737dfa64d6
Alexandre BRUN-GIGLIO <alexandre.brungiglio@nextalys.com>
2022-03-12 02:07:46 +0100

    input

--- front/src/components/tables/table.module.ts
+++ front/src/components/tables/table.module.ts
@@ -1,8 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TuiButtonModule, TuiDialogModule, TuiLoaderModule, TuiTableModeModule } from '@taiga-ui/core';
+import { TuiActionModule } from '@taiga-ui/kit';
 import { BasePageModulesList } from '../../app/app.module';
 import { TableComponent } from './table.component';
 
 @NgModule({
+    imports: [
+        TuiTableModeModule,
+        CommonModule,
+        FontAwesomeModule,
+        TuiLoaderModule,
+        TuiDialogModule,
+        TuiActionModule,
+        TuiButtonModule,
+    ],
     declarations: [
         TableComponent,
     ],
