444803fd5523e207ba4d4d87a9247a7fe12752b8
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-03-18 01:02:03 +0100

    create cron task

--- back/src/modules/stats/stats.service.ts
+++ back/src/modules/stats/stats.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AppErrorWithMessage } from '../../base/app-error';
 import { ApplicationBaseModelService } from '../../base/base-model.service';
+import { UsersService } from '../users/users.service';
 import { GetStatResponse, GetStatsResponse, StatDto } from './stat-dto';
 import { Stat } from './stat.entity';
 
@@ -10,6 +13,7 @@ export class StatsService extends ApplicationBaseModelService<Stat, StatDto, Get
     constructor(
         @InjectRepository(Stat)
         public readonly repository: Repository<Stat>,
+        public readonly userService: UsersService,
     ) {
         super();
         this.modelOptions = {
@@ -22,5 +26,30 @@ export class StatsService extends ApplicationBaseModelService<Stat, StatDto, Get
             archiveField: 'disabled',
             archiveFieldValue: true,
         };
+    }
+
+    @Cron(CronExpression.EVERY_DAY_AT_1AM, {
+        name: 'user-stats',
+        timeZone: 'Europe/Paris'
+    })
+    async setUserStats() {
+        try {
+            const getUserLenght = await this.userService.findAll();
+            if (!getUserLenght.success)
+                throw new AppErrorWithMessage(getUserLenght.message);
+            const userLength = getUserLenght.users.length;
+            const findUserStat = await super.findOne({ where: { label: 'userStat' } });
+            let stat = {} as StatDto;
+            if (findUserStat.success && findUserStat.stat?.id) {
+                stat = findUserStat.stat;
+                stat.value = userLength;
+            } else {
+                stat = { label: 'userStat', value: userLength, archived: false };
+            }
+            await super.createOrUpdate(stat);
+            console.log("Called every 10 seconds", stat);
+        } catch (error) {
+            throw new AppErrorWithMessage(error);
+        }
     }
 }
