c0b838ccbad1679997bb670303202e1e7a822d58
VPS Alexandre BRUN-GIGLIO <alexandre.brungiglio@nextalys.com>
2022-03-21 21:15:23 +0000

    fix

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiBaseUrl: 'https://api-alexandrebrungiglio.fr/taiga',
-  version: '1.0.5',
-=======
-  apiBaseUrl: 'http://localhost:3088',
   version: '1.0.6',
->>>>>>> b7c71e5655fd387eb51882d4b91f76cbce005d26
 };
