2939bb7158d1f44e966dc746886b1b9ca6b48ce6
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-03-31 17:09:09 +0200

    fix speed load page

--- front/src/pages/admin/users/edit-user.page.ts
+++ front/src/pages/admin/users/edit-user.page.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { TuiContextWithImplicit, TuiIdentityMatcher, TuiStringHandler } from '@taiga-ui/cdk';
 import { TuiDialogService } from '@taiga-ui/core';
-import { firstValueFrom } from 'rxjs';
-import { UserDto, UserRoleDto, UsersRolesService, UsersService } from '../../../providers/api-client.generated';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { GenericResponse, GetUserResponse, UserDto, UserRoleDto, UsersRolesService, UsersService } from '../../../providers/api-client.generated';
 import { BaseComponent } from '../../../utils/base/base.component';
 
 @Component({
@@ -12,13 +12,13 @@ import { BaseComponent } from '../../../utils/base/base.component';
     templateUrl: './edit-user.page.html',
     styleUrls: ['./edit-user.page.scss'],
     encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditUserPage extends BaseComponent implements OnInit {
     user = {} as UserDto;
     backIcon = faAngleLeft;
     userRolesList = [] as UserRoleDto[];
     tempUserRole = 'admin';
+    userId!: string;
     constructor(
         private router: Router,
         private route: ActivatedRoute,
@@ -31,30 +31,37 @@ export class EditUserPage extends BaseComponent implements OnInit {
     }
 
     async init() {
-        let userId = '';
+        this.userId = '';
         this.route.params.subscribe((param) => {
-            userId = param['id'];
+            this.userId = param['id'];
         });
         this.user.disabled = false;
-        this.loading = true;
-        if (userId === 'new') {
-            this.loading = false;
-            return;
-        }
-        const getUserResponse = await firstValueFrom(this.userService.getUser(userId));
-        const getRolesResponse = await firstValueFrom(this.userRoleService.getUserRoles());
-        this.loading = false;
-        if (!getRolesResponse.success) {
-            this.dialogService.open(getRolesResponse.message!).subscribe();
-        }
-        this.userRolesList = getRolesResponse.userRoles;
-        if (!getUserResponse.success) {
-            this.dialogService.open(getUserResponse.message!).subscribe();
-        }
-        this.user = getUserResponse.user;
     }
 
     async ngOnInit() {
+        if (this.userId === 'new') {
+            return;
+        }
+        else {
+            this.loading = true;
+            const getUserResponse = await firstValueFrom(this.userService.getUser({ id: this.userId }));
+            const getRolesResponse = await firstValueFrom(this.userRoleService.getUserRoles({}));
+            this.loading = false;
+            if (!getRolesResponse.success) {
+                this.dialogService.open(getRolesResponse.message!).subscribe();
+            }
+            this.userRolesList = getRolesResponse.userRoles;
+            if (!getUserResponse.success) {
+                this.dialogService.open(getUserResponse.message!).subscribe();
+            }
+            if (getUserResponse.success)
+                this.user = getUserResponse.user;
+        }
+    }
+
+    setUser(response: GetUserResponse) {
+        this.user = response.user;
+        this.loading = false;
 
     }
 
@@ -66,7 +73,7 @@ export class EditUserPage extends BaseComponent implements OnInit {
         if (!this.hasPendingModifications)
             return
         this.loading = true;
-        const saveResponse = await firstValueFrom(this.userService.createOrUpdateUser(this.user));
+        const saveResponse = await firstValueFrom(this.userService.createOrUpdateUser({ userDto: this.user }));
         this.loading = false;
         if (!saveResponse.success) {
             this.dialogService.open(saveResponse.message!).subscribe();
@@ -75,10 +82,9 @@ export class EditUserPage extends BaseComponent implements OnInit {
         this.hasPendingModifications = false;
     }
 
-    readonly stringify: TuiStringHandler<UserRoleDto | TuiContextWithImplicit<UserRoleDto>> = item =>
-        'role' in item ? item.label! : item.$implicit.role;
+    // readonly stringify: TuiStringHandler<UserRoleDto | TuiContextWithImplicit<UserRoleDto>> = item =>
+    //     'role' in item ? item.label! : item.$implicit.role;
 
-    readonly identityMatcher: TuiIdentityMatcher<UserRoleDto> = (roleA, roleB) =>
-        roleA.id === roleB.id;
-
+    // readonly identityMatcher: TuiIdentityMatcher<UserRoleDto> = (roleA, roleB) =>
+    //     roleA.id === roleB.id;
 }
