08fa65c0f7d6ef8ee8ca19c2af79a92c6402ea93
Alexandre BG <alexandre.brungiglio@nextalys.com>
2022-04-01 19:58:40 +0200

    jwt strategy

--- back/src/auth/guards/jwt.strategy.ts
+++ back/src/auth/guards/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: Environment.jwt_secret,
+            secretOrKey: Environment.access_token_secret,
         });
     }
 
